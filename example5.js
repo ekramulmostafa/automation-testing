150b474ba07be09cad0f86eaa28b24ce596ece8c
ekramulmostafa <ekramul.mostafa@gmail.com>
2018-07-16 18:33:36 +0600

    asd

--- example5.js
+++ example5.js
@@ -2,6 +2,7 @@ const {By, Key, until} = require('selenium-webdriver');
 const {suite} = require('selenium-webdriver/testing');
 const assert = require('assert');
 
+
 suite(function(env) {
     describe('Bongo Testing', function() {
         let driver;
@@ -9,12 +10,13 @@ suite(function(env) {
         beforeEach(async function() {
             driver = await env.builder().build();
             driver.get('https://www.google.com/');
+
             // driver.get('https://www.google.com/gmail/about/');
         });
 
         afterEach(() => driver.quit());
 
-        it('demo1', async function() {
+        /*it('demo1', async function() {
 
             await driver.findElement(By.name('q')).sendKeys('gmail', Key.RETURN);
 
@@ -36,13 +38,13 @@ suite(function(env) {
             await pass.sendKeys('Ekramul@109', Key.ENTER);
             // await driver.findElement(By.css('#passwordNext')).click();
 
-            await driver.sleep(15000);
+            await driver.sleep(7000);
             await driver.wait(until.titleContains('Bongo Holdings PTE Limited Mail'), 5000);
 
             console.log('done');
 
-            await driver.findElements(By.css('.zA')).then(function (elements) {
-                elements.map(function (el) {
+            await driver.findElements(By.css('.zA')).then(async function (elements) {
+                elements.map(async function (el) {
 
                     el.findElements(By.css('.zF')).then(function (elements) {
                         elements.map(function (el) {
@@ -65,32 +67,112 @@ suite(function(env) {
                         console.log(txt);
                     });
 
-                    el.findElement(By.css('.bog > span')).click();
+                    //el.findElement(By.css('.bog > span')).click();
+
+
+                    // await driver.sleep(2000);
+                    // await driver.navigate().back();
+                    // await driver.sleep(3000);
                 })
 
             })
 
             await driver.sleep(5000);
 
-        });
-
-        /*it('demo2', async function(){
-
-            await driver.wait(until.elementLocated(By.css('a.gmail-nav__nav-link.gmail-nav__nav-link__sign-in')), 15000);
-            await driver.findElement(By.css('a.gmail-nav__nav-link.gmail-nav__nav-link__sign-in')).click()
-            await driver.sleep(10000);
         });*/
 
-        /*it('demo3', async function(){
 
-            await driver.findElements(By.css('ul li')).then(function (elm) {
-                elm.map(function (el) {
-                    el.getText().then(function (txt) {
-                        console.log(txt);
-                    })
+        it('demo2', async function() {
+
+            await driver.findElement(By.name('q')).sendKeys('gmail', Key.RETURN);
+
+            await driver.findElements(By.css('.bkWMgd')).then(async function (elements) {
+                elements.map(function (elm) {
+                    elm.findElement(By.linkText('Gmail - Google')).click();
                 })
             })
 
-        });*/
+            await driver.wait(until.elementLocated(By.css('a.gmail-nav__nav-link.gmail-nav__nav-link__sign-in')));
+            await driver.findElement(By.css('a.gmail-nav__nav-link.gmail-nav__nav-link__sign-in')).click()
+
+            let email = await driver.wait(until.elementLocated(By.name('identifier')));
+            await email.sendKeys('ekramul@bongobd.com', Key.ENTER);
+            // await driver.findElement(By.css('#identifierNext')).click();
+
+            await driver.sleep(1000);
+            let pass = await driver.wait(until.elementLocated(By.name('password')));
+            await pass.sendKeys('Ekramul@109', Key.ENTER);
+            // await driver.findElement(By.css('#passwordNext')).click();
+
+            await driver.sleep(1000);
+            await driver.wait(until.titleContains('Bongo Holdings PTE Limited Mail'), 2000);
+
+            var ListMails = null;
+
+            await driver.findElements(By.css('.bog > span')).then(async function (elements) {
+                ListMails = elements;
+            })
+
+
+            for(var i = 0; i < ListMails.length; i++){
+
+                if(i == 5){
+                    break;
+                }
+                await driver.findElements(By.css('.bog > span')).then(async function (elements) {
+                    ListMails = elements;
+                })
+
+                ListMails[i].click();
+                
+                
+                await driver.sleep(1000);
+                await driver.navigate().back();
+            }
+
+
+            /*ListMails[1].click();
+            await driver.sleep(1000);
+            await driver.navigate().back();
+
+
+            await driver.findElements(By.css('.bog > span')).then(async function (elements) {
+                ListMails = elements;
+            })
+
+            ListMails[2].click();
+            await driver.sleep(1000);
+            await driver.navigate().back();
+
+
+            await driver.findElements(By.css('.bog > span')).then(async function (elements) {
+                ListMails = elements;
+            })
+
+            ListMails[3].click();
+            await driver.sleep(1000);
+            await driver.navigate().back();
+
+
+            await driver.findElements(By.css('.bog > span')).then(async function (elements) {
+                ListMails = elements;
+            })
+
+            ListMails[4].click();
+            await driver.sleep(1000);
+            await driver.navigate().back();
+*/
+
+
+            // ListMails[1].click();
+            // await driver.sleep(1000);
+            // driver.navigate().back();
+
+
+            await driver.sleep(5000);
+
+        });
+
+
     });
 });
